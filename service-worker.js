fb0f873c7a8f01d3c0096d2a6544e2ebf67e2405
YansYansYans <62288526+YansYansYans@users.noreply.github.com>
2020-09-04 09:58:17 +0000

    Deploying to gh-pages from  @ 523f069d244a7e0476aa19a7ac48cd6638039096 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28f077d36964e7986af246527f60e76b.js"
+  "/yannisdevdesign/precache-manifest.6354ec5ad5c21b6b1c568cd4459097ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yannisdevdesign/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
